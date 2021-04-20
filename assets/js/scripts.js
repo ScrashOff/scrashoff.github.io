55c162a7c7cc9247e86e9eaa1d75a9725d83fd91
Fellipe "ScrashOff" Orsini <fellipe.orsini@hotmail.com>
2021-04-20 12:54:37 -0300

    Update js\scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,49 +1,49 @@
 ﻿var meusProjetos = [
     {
-		'name': 'Troia Games (2011)',
-		'image': 'assets/images/troia-games.big.png',
-		'description': 'Um site de venda de chaves para jogos online, na epóca trabalhei com mais um designer. Cuidei de todo desenvolvimento do backend e frontend e design.'
+		'name': 'SGNE - Sistema de gerenciamento de negócios especializado (2020)',
+		'image': 'assets/images/painel.domma.store.png',
+		'description': 'Plataforma para gestão empresarial, inclui cadastro de clientes, transportadoras, funcionário, produtos com ou sem variações, controle de estoque completo, geração de código de barras, controle financeiro, integração com ecommerce, relatórios DRE e muito mais.'
 	},
-	{
-		'name': 'Legendado (2015)',
-		'image': 'assets/images/legendado.min.png',
-		'description': 'Plataforma online que disponibilizava legenda para vários filmes, séries e documentários nacionais e internacionais. No projeto, cuidei do design e programação dos sistemas, incluindo painel administrativo para as necessidades do site.'
+    {
+		'name': 'DOMMA - Ecommerce (2020)',
+		'image': 'assets/images/dommastore.com.br.png',
+		'description': 'Plataforma estilo ecommerce que permite estilização, adição de categorias, produtos, totalmente integrado com o Painel de Gestão Empresarial próprio e possível abertura para franqueados online. E é totalmente responsivo.'
 	},
-	{
-		'name': 'Alcance Social (2016)',
-		'image': 'assets/images/alcancesocial.min.png',
-		'description': 'Uma plataforma onde donos de páginas no facebook podem lucrar com seus seguidores, impulsionando suas postagens e gerando lucro com seus curtidores, nesse projeto cuideo do design e programação dos sistemas.'
-	},
-	{
-		'name': 'Encurta me (2016)',
-		'image': 'assets/images/encurtame.big.png',
-		'description': 'Um sistema que permite com que os usuários que encurtem as suas urls no site ganhem dinheiro a cada 1000 acessos no link. Cuidei de todo desenvolvimento desde design, frontend, backend e um painel administrativo exclusivo para as necessidades do site. Possui integração com PayPal.'
-	},
-	{
-		'name': 'FollowInsta (2017-2019)',
-		'image': 'assets/images/followinsta.big.png',
-		'description': 'Uma plataforma que permite que os usuários ganhem seguidores no instagram, no projeto, cuidei do desenvolvimento do layout, codificação frontend e backend, também possui um painel exclusivo para gerenciamento das contas, resoluão de problemas e possui integração com o PagSeguro.'
-	},
-	{
-		'name': 'Curtidas TT (2019)',
-		'image': 'assets/images/curtidastt.big.png',
-		'description': 'Plataforma que permite usuários do twitter ganhem curtidas em seus posts, também cuidei do frontend e backend no projeto todo. Possui painel administrativo próprio.'
-	},
-	{
+    {
 		'name': 'Potatos Roleplay (2019)',
 		'image': 'assets/images/followinsta.big.png',
 		'youtube': 'https://www.youtube.com/embed/ppox7kbFa_o',
 		'description': 'A primeira comunidade de Roleplay no GTA 5 na plataforma fiveM, cuidei de todo desenvolvimento do novo servidor que possui cerca de 50 mil linhas de código, incluindo códigos do HUD.'
 	},
-	{
-		'name': 'DOMMA - Ecommerce (2020)',
-		'image': 'assets/images/dommastore.com.br.png',
-		'description': 'Plataforma estilo ecommerce que permite estilização, adição de categorias, produtos, totalmente integrado com o Painel de Gestão Empresarial próprio e possível abertura para franqueados online. E é totalmente responsivo.'
+    {
+		'name': 'Curtidas TT (2019)',
+		'image': 'assets/images/curtidastt.big.png',
+		'description': 'Plataforma que permite usuários do twitter ganhem curtidas em seus posts, também cuidei do frontend e backend no projeto todo. Possui painel administrativo próprio.'
 	},
-	{
-		'name': 'SGNE - Sistema de gerenciamento de negócios especializado (2020)',
-		'image': 'assets/images/painel.domma.store.png',
-		'description': 'Plataforma para gestão empresarial, inclui cadastro de clientes, transportadoras, funcionário, produtos com ou sem variações, controle de estoque completo, geração de código de barras, controle financeiro, integração com ecommerce, relatórios DRE e muito mais.'
+    {
+		'name': 'FollowInsta (2017-2019)',
+		'image': 'assets/images/followinsta.big.png',
+		'description': 'Uma plataforma que permite que os usuários ganhem seguidores no instagram, no projeto, cuidei do desenvolvimento do layout, codificação frontend e backend, também possui um painel exclusivo para gerenciamento das contas, resoluão de problemas e possui integração com o PagSeguro.'
+	},
+    {
+		'name': 'Encurta me (2016)',
+		'image': 'assets/images/encurtame.big.png',
+		'description': 'Um sistema que permite com que os usuários que encurtem as suas urls no site ganhem dinheiro a cada 1000 acessos no link. Cuidei de todo desenvolvimento desde design, frontend, backend e um painel administrativo exclusivo para as necessidades do site. Possui integração com PayPal.'
+	},
+    {
+		'name': 'Alcance Social (2016)',
+		'image': 'assets/images/alcancesocial.min.png',
+		'description': 'Uma plataforma onde donos de páginas no facebook podem lucrar com seus seguidores, impulsionando suas postagens e gerando lucro com seus curtidores, nesse projeto cuideo do design e programação dos sistemas.'
+	},
+    {
+		'name': 'Legendado (2015)',
+		'image': 'assets/images/legendado.min.png',
+		'description': 'Plataforma online que disponibilizava legenda para vários filmes, séries e documentários nacionais e internacionais. No projeto, cuidei do design e programação dos sistemas, incluindo painel administrativo para as necessidades do site.'
+	},
+    {
+		'name': 'Troia Games (2011)',
+		'image': 'assets/images/troia-games.big.png',
+		'description': 'Um site de venda de chaves para jogos online, na epóca trabalhei com mais um designer. Cuidei de todo desenvolvimento do backend e frontend e design.'
 	},
 ];
 
